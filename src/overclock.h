24ebf6d97d4f8b9e4a30bb808a39b4fc710f2069
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-02-02 21:17:56 +0100

    reorganize the code, remove sync from settle, reduce pipeline delay during overclocking process

--- src/overclock.h
+++ src/overclock.h
@@ -16,37 +16,52 @@
 #define PLL_BASE_FREQ         37
 #define PLL_DEN               19
 
+#define updatePLLMultiplier(var)                    \
+{                                                   \
+  const u32 lsb = (var/*_num*/) << 8 | PLL_DEN;     \
+  const u32 multiplier = (PLL_MUL_MSB << 16) | lsb; \
+  hw(0xbc1000fc) = multiplier;                      \
+  sync();                                           \
+  /*do {*/                                          \
+  /*  delayPipeline();*/                            \
+  /*} while(hw(0xbc1000fc) != multiplier);*/        \
+}
+
+// set bit bit 7 to apply index, wait until hardware clears it
+#define updatePLLControl()                          \
+{                                                   \
+  hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;          \
+  do {                                              \
+    delayPipeline();                                \
+  } while (hw(0xbc100068) != PLL_RATIO_INDEX);      \
+}
+
 static inline void setOverclock() {
   
   // note: needs to be 333 to be able to reach 444mhz
   const int INITIAL_FREQUENCY = DEFAULT_FREQUENCY;  
-  scePowerSetClockFrequency(INITIAL_FREQUENCY, INITIAL_FREQUENCY, INITIAL_FREQUENCY/2);
   
-  const u32 num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / PLL_BASE_FREQ);
   u32 _num = (u32)(((float)(INITIAL_FREQUENCY * PLL_DEN)) / PLL_BASE_FREQ);
+  const u32 num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / PLL_BASE_FREQ);
+
+  scePowerSetClockFrequency(INITIAL_FREQUENCY, INITIAL_FREQUENCY, INITIAL_FREQUENCY/2);
   
   int intr;
   suspendCpuIntr(intr);
-  resetDomains();
   
-  // set bit bit 7 to apply index, wait until hardware clears it
-  hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;
-  do {
-    delayPipeline();
-  } while (hw(0xbc100068) != PLL_RATIO_INDEX);
+  resetDomains();
+  updatePLLControl();
 
   // loop until the numerator reaches the target value,
   // and so, progressively increasing clock frequencies
   while (_num <= num) {
-    const u32 lsb = _num << 8 | PLL_DEN;
-    const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
-    hw(0xbc1000fc) = multiplier;
-    delayPipeline();
+    updatePLLMultiplier(_num);
     _num++;
   }
-
+  
   settle();
   resetDomains();
+  
   resumeCpuIntr(intr);
 }
 
@@ -74,26 +89,16 @@ static inline void cancelOverclock() {
     
     while (_num > num) {
       _num--;
-      const u32 lsb = _num << 8 | PLL_DEN;
-      const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
-      hw(0xbc1000fc) = multiplier;
-      delayPipeline();
+      updatePLLMultiplier(_num);
     }
 
-    hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;
-    do {
-      delayPipeline();
-    } while (hw(0xbc100068) != PLL_RATIO_INDEX);
+    updatePLLControl();
 
     settle();
     resetDomains();
   }
   
   resumeCpuIntr(intr);
-
-  //if (overclocked) {
-  //  scePowerSetClockFrequency(TARGET_FREQUENCY, TARGET_FREQUENCY, TARGET_FREQUENCY/2);
-  //}
 }
 
 #endif
