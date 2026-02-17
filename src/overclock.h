9dd8258274b00e8bdbd6b326c43f47fe8518d9c2
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-02-17 07:11:49 +0100

    port improvments from stress tester to overclock plugin

--- src/overclock.h
+++ src/overclock.h
@@ -39,44 +39,97 @@ static int THEORETICAL_FREQUENCY  = 444;
     do {                                            \
       delayPipeline();                              \
     } while (hw(0xbc100068) & 0x80);                \
+    sync();                                         \
   }                                                 \
 }
 
-static inline void rampUpPLLRatio() {
-  sceKernelDelayThread(100);
+static inline void adjustPLLMultiplier() {
   
-  int intr, state;
-  state = sceKernelSuspendDispatchThread();
-  suspendCpuIntr(intr);
+  const u32 defaultNum = (u32)(((float)(DEFAULT_FREQUENCY * PLL_DEN)) / ((float)PLL_BASE_FREQ));
+  hw(0xbc1000fc) = (PLL_MUL_MSB << 16) | (defaultNum << 8) | PLL_DEN;
+  settle();
+}
+
+static inline void adjustPLLRatio() {
   
   u32 index = hw(0xbc100068) & 0x0f;
-  if (index < 5) {
+  sync();
+
+  if (index != 5) {
     
-    const u32 defaultNum = (u32)(((float)(DEFAULT_FREQUENCY * PLL_DEN)) / ((float)PLL_BASE_FREQ));
-    hw(0xbc1000fc) = (PLL_MUL_MSB << 16) | (defaultNum << 8) | PLL_DEN;
-    sync();
-    
-    while (index <= 5) {
+    const int step = (index > 5) ? -1 : 1;
+    while (((step < 0) == (index > 5)) || index == 5) {
+        
       hw(0xbc100068) = 0x80 | index;
-      sync();  
+      sync();
+      
       do {
         delayPipeline();
       } while ((hw(0xbc100068) & 0x80));
       settle();
-      index++;
+      
+      index += step;
     }
   }
   
+}
+
+static inline void adjustDomainRatios() {
+  
+  const u32 cpu = hw(0xbc200000);
+  const u32 bus = hw(0xBC200004);
+  sync();
+  
+  u32 cpuDen = cpu & 0x1ff;
+  u32 cpuNum = (cpu >> 16) & 0x1ff;
+  u32 busDen = bus & 0x1ff;
+  u32 busNum = (bus >> 16) & 0x1ff;
+  
+  hw(0xbc200000) = (cpuNum << 16) | cpuDen;
+  hw(0xBC200004) = (busNum << 16) | busDen;
+  settle();
+    
+  const int step = 18;
+  while ((cpuNum & cpuDen & busNum & busDen) != 0x1ff) {
+    
+    const u32 nextCpuNum = cpuNum + step;
+    const u32 nextCpuDen = cpuDen + step;
+    const u32 nextBusNum = busNum + step;
+    const u32 nextBusDen = busDen + step;
+    
+    cpuNum = (nextCpuNum > 0x1ff) ? 0x1ff : nextCpuNum;
+    cpuDen = (nextCpuDen > 0x1ff) ? 0x1ff : nextCpuDen;
+    busNum = (nextBusNum > 0x1ff) ? 0x1ff : nextBusNum;
+    busDen = (nextBusDen > 0x1ff) ? 0x1ff : nextBusDen;
+    
+    hw(0xbc200000) = (cpuNum << 16) | cpuDen;
+    hw(0xBC200004) = (busNum << 16) | busDen;
+    settle();
+  }
+}
+
+void adjustInitialFrequencies() {
+  
+  sceKernelDelayThread(100);
+
+  int intr, state;
+  state = sceKernelSuspendDispatchThread();
+  suspendCpuIntr(intr);
+
+  adjustPLLMultiplier();
+  adjustPLLRatio();
+  adjustDomainRatios();
+
   resumeCpuIntr(intr);
   sceKernelResumeDispatchThread(state);
-  sceKernelDelayThread(100);
 }
+
 
 
 static inline void setOverclock() {
   
   scePowerSetClockFrequency(DEFAULT_FREQUENCY, DEFAULT_FREQUENCY, DEFAULT_FREQUENCY/2);
-  rampUpPLLRatio();
+  adjustInitialFrequencies();
   
   int defaultFreq = DEFAULT_FREQUENCY;
   const int freqStep = PLL_BASE_FREQ / 2;
@@ -122,19 +175,25 @@ static inline int cancelOverclock() {
   state = sceKernelSuspendDispatchThread();
   suspendCpuIntr(intr);
   
-  const u32 pllCtl = hw(0xbc100068);
-  const u32 pllMul = hw(0xbc1000fc);
+  const u32 pllCtl = hw(0xbc100068) & 0x0f;
+  const u32 pllMul = hw(0xbc1000fc) & 0xffff;
   sync();
+  
+  resumeCpuIntr(intr);
+  sceKernelResumeDispatchThread(state);
   
   const float n = (float)((pllMul & 0xff00) >> 8);
   const float d = (float)((pllMul & 0x00ff));
   const float m = (d > 0.0f) ? (n / d) : 9.0f;
   const int overclocked = ((pllCtl & 5) && (m > 9.0f)) ? 1 : 0;
-  
+  sceKernelDelayThread(1000);
+
   //const u32 pllMul = hw(0xbc1000fc); sync();
   //const int overclocked = pllMul & (1 << PLL_CUSTOM_FLAG);
   
   if (overclocked) {
+    state = sceKernelSuspendDispatchThread();
+    suspendCpuIntr(intr);
     
     updatePLLControl();
 
@@ -143,10 +202,10 @@ static inline int cancelOverclock() {
       _num--;
     }
     settle();
+    
+    resumeCpuIntr(intr);
+    sceKernelResumeDispatchThread(state);
   }
-  
-  resumeCpuIntr(intr);
-  sceKernelResumeDispatchThread(state);
   
   return overclocked;
 }
